b7b149f3190945bab1cb024e24db38cc780e0103
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-05-02 00:58:38 +0100

    Implement sign-in functionality with input validation and toast notifications

---
+++ signin.js
@@ -0,0 +1,62 @@
+const toast = (text, background, color, position = "right") => {
+    Toastify({
+        text,
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position, // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background,
+            color,
+        },
+        onClick: function () { } // Callback after click
+    }).showToast();
+
+}
+
+
+const gotten = JSON.parse(localStorage.getItem("users"));
+const signIn = () => {
+    if(inMail.value == "" || passwordIn.value == ""){  
+        alert("Please fill all the fields")
+    }
+
+    let found = false;
+
+    for (let i = 0; i < gotten.length; i++) {
+        // console.log(gotten[i].userEmail);
+        if(inMail.value == gotten[i].userEmail && passwordIn.value == gotten[i].pWord)  {
+            found = true;
+            // window.location.href = 'index.html'
+            
+        } 
+    }
+
+    if (found == false) {
+        toast("Invalid email or password 😒", "red", "white")
+    } else {
+        toast("Login successful 🎉", "green", "white")
+        sub.innerHTML = `
+        <div class="dot-spinner">
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+    <div class="dot-spinner__dot"></div>
+</div>
+        `
+        setTimeout(() => {
+            sub.innerHTML = 'Submit'
+        }, 3000)
+        
+    }
+   // window.location.href = 'index.html' 
+}
+
+
+
